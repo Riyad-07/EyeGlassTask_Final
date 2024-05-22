0c2d7afbec303337421f4b7c9d2fcd78f97e0304
Riyad-07 <samimsamim1998@gmail.com>
2024-05-22 23:31:07 +0600

    ready project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    
+  
+});
+
+
+          
+$('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    
+  
+});
+          
